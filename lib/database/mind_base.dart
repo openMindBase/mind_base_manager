464fb8ce9b85d27d8ee66cab68f092942c5cdcd0
matthiasweigt <m.w.weigt@gmail.com>
2023-03-21 14:09:07 +0100

    mind_base.dart

---
+++ lib/database/mind_base.dart
@@ -0,0 +1,29 @@
+// @author Matthias Weigt 21.03.23
+
+
+import '../domain/entities/learning_goal.dart';
+
+abstract class MindBase{
+  static MindBase? currentMindBase;
+
+  /// Initializes the [MindBase]database in use.
+  /// Must be called before accessing the database.
+  static init(MindBase mindBase) => currentMindBase = mindBase;
+
+  /// Access to the current [MindBase].
+  static MindBase get instance{
+    if(currentMindBase==null) {
+      throw StateError("The MindBase is not initialized yet. Use MindBase.init for Initialization.");
+    }
+    return currentMindBase!;
+  }
+
+
+
+  /// Reads a single [LearningGoal] by the id of the goal.
+  Future<LearningGoal> readLearningGoal(String id);
+
+  /// Reads all [LearningGoal]s.
+  Future<List<LearningGoal>> readAllLearningGoals({bool printStats=false});
+
+}

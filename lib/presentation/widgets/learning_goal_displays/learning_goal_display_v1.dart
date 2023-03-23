b3ed6730e9d5f65d97f8ed4cbb1dd736f69ef478
matthiasweigt <m.w.weigt@gmail.com>
2023-03-23 17:58:21 +0100

    learning_goal_display_v1.dart

---
+++ lib/presentation/widgets/learning_goal_displays/learning_goal_display_v1.dart
@@ -0,0 +1,98 @@
+// @author Matthias Weigt 15.02.23
+// All rights reserved ©2023
+
+import 'package:flutter/material.dart';
+import 'package:lean_ui_kit/presentation/widgets/lean_spaced_column.dart';
+
+import '../../../domain/entities/exercise.dart';
+import '../../../domain/entities/learning_goals_and_structures/learning_goal.dart';
+import '../text/lean_tex.dart';
+
+class LearningGoalDisplayV3 extends StatefulWidget {
+  const LearningGoalDisplayV3(
+      {super.key,
+      required this.learningGoal,
+      required this.width,
+      required this.height,
+      this.showTitle = false});
+
+  final LearningGoal? learningGoal;
+  final double width;
+  final double? height;
+  final bool showTitle;
+
+  @override
+  _LearningGoalDisplayV3State createState() => _LearningGoalDisplayV3State();
+}
+
+class _LearningGoalDisplayV3State extends State<LearningGoalDisplayV3> {
+  bool showFront = true;
+
+  String goalId = "";
+
+  final TextEditingController titleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.learningGoal != null) {
+      if (goalId != widget.learningGoal!.id) {
+        showFront = true;
+        goalId = widget.learningGoal!.id;
+      }
+    }
+
+    Widget child = Container(
+      padding: const EdgeInsets.all(20),
+      width: widget.width,
+      height: widget.height,
+      child: LeanSpacedColumn(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            if (widget.showTitle)
+              widget.learningGoal != null
+                  ? Text(
+                      widget.learningGoal!.title,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    )
+                  : const SizedBox(),
+            widget.learningGoal != null
+                ? LeanTex(
+                    // height: widget.height!.toInt() - 40,
+                    texData: _getTex(),
+                  )
+                : const SizedBox()
+          ]),
+    );
+
+    bool hasExercises = widget.learningGoal == null
+        ? false
+        : widget.learningGoal!.exercises.isEmpty
+            ? false
+            : true;
+
+    return Card(
+      child: hasExercises
+          ? InkWell(
+              onTap: () => setState(() {
+                showFront = !showFront;
+              }),
+              child: child,
+            )
+          : child,
+    );
+  }
+
+  String _getTex() {
+    Exercise? e = widget.learningGoal!.randomExercise();
+    if (e == null) {
+      return "Beschreibung:\n${widget.learningGoal!.description}";
+    }
+
+    if (showFront) {
+      return "Aufgabe:\n${e.question}";
+    }
+    return "Lösung:\n${e.answer}";
+  }
+}
+
+

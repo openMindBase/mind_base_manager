c4899a550aecfd67978da445e4da43242aefe6ed
matthiasweigt <m.w.weigt@gmail.com>
2023-03-23 17:58:33 +0100

    student metadata

---
+++ lib/domain/entities/persons/student_metadata.dart
@@ -0,0 +1,33 @@
+// @author Matthias Weigt 13.02.23
+// All rights reserved ©2023
+
+/// The metadata to display at the StudentSelectionScreen
+class StudentMetadata {
+  const StudentMetadata(this.id, {this.name});
+
+  static const StudentMetadata example = StudentMetadata("testStudent12345",name: "Mira Bellenbaum");
+
+
+
+  static StudentMetadata fromJson(Map<String,dynamic> data) {
+    return StudentMetadata(data["id"],name: data["name"],);
+  }
+
+
+  final String id;
+  final String? name;
+
+  String toFullString() {
+    return 'StudentMetadata{id: $id, name: $name}';
+  }
+
+  @override
+  String toString() {
+    return "${name ?? id}}";
+  }
+
+
+
+
+
+}

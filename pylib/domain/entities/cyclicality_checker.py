172d47e03963b67645821aabf4ad05cceda32cc1
matthiasweigt <m.w.weigt@gmail.com>
2023-04-16 13:54:05 +0200

    optimize imports

--- pylib/domain/entities/cyclicality_checker.py
+++ pylib/domain/entities/cyclicality_checker.py
@@ -5,10 +5,12 @@ checks a list of files for the cyclical property, reports if it exists
 in: DiGraph
 out: None
 """
-def checkCyclicality(Graph :DiGraph) -> None:
+
+
+def checkCyclicality(Graph: DiGraph) -> None:
     print("processed " + str(len(Graph.edges)) + " edges in cyclicality checker")
     try:
-        if isinstance(find_cycle(Graph),list):
+        if isinstance(find_cycle(Graph), list):
             raise Exception("cycles found with following path:" + str(find_cycle(Graph)))
     except NetworkXNoCycle:
         print("no cycles found")
